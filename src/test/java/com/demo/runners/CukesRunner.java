1409edd1ba8240a91963d83771e9c63d3b32bc1c
ErnieB20 <66183616+ErnieB20@users.noreply.github.com>
2025-03-03 19:11:32 -0600

    Update files
    Add plugins and dependencies

--- src/test/java/com/demo/runners/CukesRunner.java
+++ src/test/java/com/demo/runners/CukesRunner.java
@@ -7,9 +7,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
+                "json:target/cucumber.json",
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
         },
         features = "src/test/resources/features",
         glue = "com/demo/step_definitions",
